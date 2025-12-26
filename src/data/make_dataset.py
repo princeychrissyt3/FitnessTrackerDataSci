07498bb2e7a4157622694cc59872632e419b6c1e
pe79 <pe79@kent.ac.uk>
2025-12-26 23:11:48 +0000

    Clean and update dataset processing

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,4 +1,3 @@
-
 # %%
 import pandas as pd
 from glob import glob
@@ -7,6 +6,7 @@ from glob import glob
 # --------------------------------------------------------------
 # Read single CSV file
 # --------------------------------------------------------------
+
 single_file_acc = pd.read_csv("../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Accelerometer_12.500Hz_1.4.4.csv")
 single_file_gyr = pd.read_csv("../../data/raw/MetaMotion/A-bench-heavy2-rpe8_MetaWear_2019-01-11T16.10.08.270_C42732BE255C_Gyroscope_25.000Hz_1.4.4.csv")
 # --------------------------------------------------------------
@@ -43,7 +43,6 @@ df = pd.read_csv(f)
 df["partcipant"] = participant
 df["label"] = label
 df["category"] = category
-
 # --------------------------------------------------------------
 # Read all files
 # --------------------------------------------------------------
@@ -215,4 +214,3 @@ data_resampled["set"] = data_resampled["set"].astype("int")
 data_resampled.to_pickle("../../data/interim/01_data_processed.pkl")
 
 # %%
-
