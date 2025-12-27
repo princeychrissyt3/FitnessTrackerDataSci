267e4e79fd5879de4d480d2bcd22163509698487
pe79 <pe79@kent.ac.uk>
2025-12-27 16:36:33 +0000

    Update dataset creation logic for cleaned data v2

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -167,9 +167,9 @@ data_merged.columns = [
     "gyr_x",
     "gyr_y",
     "gyr_z",
+    "participant",
     "label",
     "category",
-    "participant",
     "set",
 ]
 
@@ -188,9 +188,9 @@ sampling = {
     "gyr_x": "mean",
     "gyr_y": "mean",
     "gyr_z": "mean",
+    "participant": "last",
     "label": "last",
     "category": "last",
-    "participant": "last",
     "set": "last",
 }
 
